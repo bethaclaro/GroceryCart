f758ff379b606345d187b461e5009ad5fb4f89e0
Beth Aclaro <beth@MacbookPro15.local>
2018-04-30 22:05:39 +0800

    working barcode scanner

--- containers/ScanNew.js
+++ containers/ScanNew.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react/native'
 import { SafeAreaView, Text, View, StyleSheet, Modal } from 'react-native'
 import defStyles from './defaultStyles'
 import { Button } from 'react-native-elements'
-import { observer } from 'mobx-react/native'
-import { observable } from 'mobx'
 import { BarCodeScanner } from 'expo'
 
 
@@ -11,16 +11,21 @@ import { BarCodeScanner } from 'expo'
 export default class ScanNew extends Component {
 
     @observable scanVisible = false
+    @observable scannedBarcode = "Yo"
 
+    constructor(props) {
+        super(props)
+        this.onScanHandler = this.onScanHandler.bind(this)
+        this.onBarcodeScan = this.onBarcodeScan.bind(this)
+    }
 
     onScanHandler(e) {
-        alert("hey " + this.scanVisible)
         this.scanVisible = !this.scanVisible
     }
 
     onBarcodeScan(e) {
-        alert(e.data)
         this.scanVisible = !this.scanVisible
+        this.scannedBarcode = e.data
     }
 
     render() {
@@ -34,7 +39,7 @@ export default class ScanNew extends Component {
                 </Modal>
                 
                 <View style={{alignItems: 'center', justifyContent: 'center', flex: 2}}>
-                    <Text style={{color: 'white'}}>Yo!</Text>
+                    <Text style={{color: 'white'}}>{this.scannedBarcode}</Text>
                 </View>
                 
                 <Button  title="Scan" onPress={this.onScanHandler} />
